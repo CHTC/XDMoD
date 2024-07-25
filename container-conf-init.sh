21dd5fc343a5bd93297a996c86edcc7ae0d432fc
williamnswanson <wnswanson@wisc.edu>
2024-07-25 16:09:18 -0500

    Add ENV vars for setting mysql buffer pool and log file sizes

--- container-conf-init.sh
+++ container-conf-init.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+SERVER_MEM_BYTES=$(($MYSQL_SERVER_MEM_GIGS * 1024 * 1024 * 1024))
+export MYSQL_BUFFER_POOL_SIZE=$(($SERVER_MEM_BYTES / 2))
+export MYSQL_LOG_FILE_SIZE=$(($MYSQL_BUFFER_POOL_SIZE / 4))
+
 for file in $(cat /setup_config/config_files.txt); do cat $file | envsubst "$(cat /setup_config/config_vars.txt)" > $file; done
