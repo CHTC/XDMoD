d62f57d85db9a06b0f0f515876b3300a7644cf3e
williamnswanson <wnswanson@wisc.edu>
2024-08-20 14:53:26 -0500

    Have runtime setup script wait for mysqld to start

--- setup_config/runtime-container-setup-script.sh
+++ setup_config/runtime-container-setup-script.sh
@@ -11,7 +11,12 @@ chmod o-r $XDMOD_ADMIN_PASSWORD_PATH
 
 supervisorctl start mysql
 
-mysql --password="" -Be "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1'); SET PASSWORD FOR 'xdmod'@'localhost' = PASSWORD('$(cat $XDMOD_ADMIN_PASSWORD_PATH)'); SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$(cat $MYSQL_ROOT_PASS_PATH)'); FLUSH PRIVILEGES;"
+while [ ! $(mysqladmin ping 2>/dev/null)];
+do
+        sleep 1;
+done
+
+mysql --password="" -Be "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1'); CREATE user 'xdmod'@'localhost' identified by '$(cat $XDMOD_ADMIN_PASSWORD_PATH)'; SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$(cat $MYSQL_ROOT_PASS_PATH)'); FLUSH PRIVILEGES;"
 
 #supervisorctl start php-fpm-server-runner
 # httpd
