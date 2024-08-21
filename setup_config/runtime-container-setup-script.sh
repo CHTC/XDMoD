02baf5b8764c4005ec84021c38d46b5aeec81551
williamnswanson <wnswanson@wisc.edu>
2024-08-21 14:18:09 -0500

    Possible fix for xdmod admin password setting

--- setup_config/runtime-container-setup-script.sh
+++ setup_config/runtime-container-setup-script.sh
@@ -13,7 +13,7 @@ do
     sleep 1;
 done
 
-mysql --password="" -Be "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1'); CREATE USER IF NOT EXISTS 'xdmod'@'localhost' identified by '$(cat $XDMOD_ADMIN_PASSWORD_PATH)'; SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$(cat $MYSQL_ROOT_PASS_PATH)'); FLUSH PRIVILEGES;"
+mysql --password="" -Be "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1'); CREATE USER IF NOT EXISTS 'xdmod'@'localhost' identified by IDENTIFIED VIA mysql_native_password USING '$(cat $XDMOD_ADMIN_PASSWORD_PATH)'; SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$(cat $MYSQL_ROOT_PASS_PATH)'); FLUSH PRIVILEGES;"
 
 #supervisorctl start php-fpm-server-runner
 # httpd
